b514ad4f3dc0e9bc5c9cb0542cd34df541709ffa
mintusarker <sarkermintu707@gmail.com>
2024-06-03 01:36:18 +0600

    product API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 var jwt = require("jsonwebtoken");
 require("dotenv").config();
 
@@ -23,18 +23,69 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const userCollection = client.db("product").collection("users");
+    const ProductCollection = client.db("product").collection("product");
 
+    //save users
 
+    //users get api
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const result = await userCollection.find({}).toArray();
+      res.send(result);
+    });
 
+    //users delete api
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(filter);
+      res.send(result);
+    });
 
-   
+    // product API section
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const result = await ProductCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+      const result = await ProductCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const product = rq.body;
+      const option = { upsert: true };
+      const updateProduct = {
+        $set: {
+          price: product.price,
+          des: product.price,
+          image_url: product.price,
+        },
+      };
+      const result = await ProductCollection.updateOne(
+        filter,
+        updateProduct,
+        option
+      );
+      res.send(result);
+    });
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await ProductCollection.deleteOne(filter);
+      res.send(result);
+    });
   } finally {
-   
   }
 }
 run().catch(console.dir);
-
-
 
 app.get("/", (req, res) => {
   res.send("server running");
